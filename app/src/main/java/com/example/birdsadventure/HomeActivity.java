aaf364a092b314e1f835af6526e474d90c9f810c
naga <nnvsek@gmail.com>
2020-05-19 23:47:22 -0400

    fragments bottom navigation (working)

--- app/src/main/java/com/example/birdsadventure/HomeActivity.java
+++ app/src/main/java/com/example/birdsadventure/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.example.birdsadventure;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView bottomnavigation = findViewById(R.id.bottom_navigation);
+    bottomnavigation.setOnNavigationItemSelectedListener(navigationListener);
     }
+ private BottomNavigationView.OnNavigationItemSelectedListener navigationListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+     @Override
+     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+         Fragment selectedFragment = null ;
+         switch (item.getItemId()){
+             case R.id.home:
+                 selectedFragment = new HomeFragment();
+                 break;
+             case R.id.search:
+                 selectedFragment = new SearchFragment();
+                 break;
+             case R.id.upload:
+                 selectedFragment = new UploadFragment();
+                 break;
+             case R.id.favorite:
+                 selectedFragment = new FavoritesFragment();
+                 break;
+             case R.id.library:
+                 selectedFragment = new LibraryFragment();
+                 break;
+         }
+getSupportFragmentManager().beginTransaction().replace(R.id.frame_space, selectedFragment).commit();
+         return true;
+     }
+ };
 }
