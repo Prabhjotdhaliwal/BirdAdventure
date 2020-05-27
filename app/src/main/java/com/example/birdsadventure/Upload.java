79c3f3c1efe3e3c10f1d1bef25ed8af06656fdd1
naga <nnvsek@gmail.com>
2020-05-27 00:05:11 -0400

    Merge branch 'master' of C:\Users\naga\StudioProjects\BirdsAdventure with conflicts.

---
+++ app/src/main/java/com/example/birdsadventure/Upload.java
@@ -0,0 +1,63 @@
+package com.example.birdsadventure;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Upload extends AppCompatActivity {
+    public static final int CAMERA_CODE = 007;
+    ImageView imageView;
+    Button camerabtn , gallerybtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_upload);
+
+        imageView = findViewById(R.id.imageView);
+        camerabtn = findViewById(R.id.cameraBtn);
+        gallerybtn = findViewById(R.id.galleryBtn);
+
+        Toast.makeText(this, "hello, I am here!!!!!!!", Toast.LENGTH_SHORT).show();
+        
+        camerabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Upload.this, "camera button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void askcamerapermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
+        } else {
+            opencamera();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CAMERA_CODE){
+            if(grantResults.length < 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                opencamera();
+            } else {
+                Toast.makeText(this, "Camera permissions are needed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void opencamera() {
+        Toast.makeText(this, "opening camera", Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/birdsadventure/UploadFragment.java
+++ app/src/main/java/com/example/birdsadventure/UploadFragment.java
@@ -1,13 +1,25 @@
 package com.example.birdsadventure;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class UploadFragment extends Fragment {
+    public static final int CAMERA_CODE = 007;
+    ImageView imageView;
+    Button camerabtn , gallerybtn;
 
     public UploadFragment() {
     }
@@ -17,12 +29,53 @@ public class UploadFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_upload, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imageView = getActivity().findViewById(R.id.imageView);
+        camerabtn = getActivity().findViewById(R.id.cameraBtn);
+        gallerybtn = getActivity().findViewById(R.id.galleryBtn);
+
+        camerabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askcamerapermission();
+            }
+        });
+    }
+    private void askcamerapermission() {
+        if(ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA},CAMERA_CODE);
+        } else {
+            opencamera();
+        }
+    }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CAMERA_CODE){
+            if(grantResults.length < 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                opencamera();
+            } else {
 
+                Toast.makeText(getActivity().getApplicationContext(), "Camera permissions are needed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void opencamera() {
+
+        Toast.makeText(getActivity().getApplicationContext(), "opening camera", Toast.LENGTH_SHORT).show();
     }
 }
