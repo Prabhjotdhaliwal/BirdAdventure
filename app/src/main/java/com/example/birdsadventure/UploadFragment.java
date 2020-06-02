46f3e3640f27db343c3e395b636e84a9c165af7e
naga <nnvsek@gmail.com>
2020-06-02 10:37:33 -0400

    Capture image and append it to IMAGEVIEW on clicking on OK(No bitmap)

--- app/src/main/java/com/example/birdsadventure/UploadFragment.java
+++ app/src/main/java/com/example/birdsadventure/UploadFragment.java
@@ -1,15 +1,19 @@
 package com.example.birdsadventure;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.service.media.MediaBrowserService;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -18,17 +22,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UploadFragment extends Fragment {
     public static final int CAMERA_CODE = 007;
     public static final int CAMERA_REQUEST = 006;
+    public static final int GALLERY_REQUEST_CODE = 005;
+    public static final int WRITE_REQUEST_CODE = 112;
+    String currentPhotoPath;
 
     ImageView imageView;
-    Button camerabtn , gallerybtn;
+    Button camerabtn, gallerybtn;
 
-    public UploadFragment() {
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,46 +64,139 @@ public class UploadFragment extends Fragment {
         camerabtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Camera button is clicked", Toast.LENGTH_SHORT).show();
                 askcamerapermission();
             }
         });
+        gallerybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Gallery button is clicked", Toast.LENGTH_SHORT).show();
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, GALLERY_REQUEST_CODE);
+            }
+        });
     }
-    private void askcamerapermission() {
-        if(ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
 
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA},CAMERA_CODE);
+    private void askcamerapermission() {
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(getActivity(), permissions, WRITE_REQUEST_CODE);
         } else {
-            opencamera();
+            //The code goes here
+            // opencamera();
+            dispatchTakePictureIntent();
         }
     }
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        if(requestCode == CAMERA_CODE){
-            if(grantResults.length < 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                opencamera();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_CODE) {
+            if (grantResults.length < 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //opencamera();
+                dispatchTakePictureIntent();
             } else {
 
                 Toast.makeText(getActivity().getApplicationContext(), "Camera permissions are needed", Toast.LENGTH_SHORT).show();
             }
+        } else if (requestCode == WRITE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Granted.
+
+
+            } else {
+                //Denied.
+            }
         }
     }
 
-    private void opencamera() {
+   /* private void opencamera() {
         Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(camera, CAMERA_REQUEST);
         Toast.makeText(getActivity().getApplicationContext(), "opening camera", Toast.LENGTH_SHORT).show();
-    }
+    } */
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == CAMERA_REQUEST){
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(image);
+        // Bitmap image = (Bitmap) data.getExtras().get("data");
+        // imageView.setImageBitmap(image);
+        if (requestCode == CAMERA_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                File f = new File(currentPhotoPath);
+                imageView.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "url of image is" + Uri.fromFile(f));
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                getActivity().sendBroadcast(mediaScanIntent);
+
+            }
         }
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri contentUri = data.getData();
+                String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timestamp + "." + getFileExt(contentUri);
+                Log.d("Log", "Gallary image Uri :" + imageFileName);
+                imageView.setImageURI(contentUri);
+
+            }
+        }
+
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        // File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(getActivity().getApplicationContext(), "ERROR: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+                // Error occurred while creating the File
+                //Permission denied, BRO
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Toast.makeText(getActivity().getApplicationContext(), "IF photoFile", Toast.LENGTH_SHORT).show();
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+            }
+        }
+
 
     }
 }
